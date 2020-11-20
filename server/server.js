86bfabefce067d1d67ed73cd9e3bfe28f1e5dc80
JuanDaniel <jujohn3000@gmail.com>
2020-11-20 12:19:28 -0600

    Creacion de una carpeta de rutas

--- server/server.js
+++ server/server.js
@@ -1,73 +1,31 @@
 require('./config/config');
 const express = require('express');
-var bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
 const app = express();
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }))
  
 // parse application/json
-app.use(bodyParser.json());
-
-/*app.use(function (req, res) {
-  res.setHeader('Content-Type', 'text/plain')
-  res.write('you posted:\n')
-  res.end(JSON.stringify(req.body, null, 2))
-})*/
+app.use(bodyParser.json())
 
 app.get('/', function (req, res) {
-  res.send('<h1>Bienvenido a mi servidor REST</h1>');
+  res.send('<h1>Bienvenido a mi servidor REST localhost</h1>')
 });
 
-app.get('/usuario', function (req, res) {
-    res.json({
-      ok: 200,
-      mensaje: 'Usuarios cosultados con exito'
-    });
-  });
+app.use(require('./routers/usuario'));
 
-app.put('/usuario/:id/:nombre', function(req, res){
-  let id = req.params.id;
-  let nombre = req.params.nombre;
-
-  res.json({
-    ok: 200,
-    mensaje: 'Usuario actualizado',
-    id: id,
-    nombre: nombre
-  });
-});
-
-app.delete('/usuario/:id', function(req, res){
-  let id = req.params.id;
-
-
-  res.json({
-    ok: 200,
-    mensaje: 'Usuario elimiado',
-    id: id,
-  });
-});
-   
-app.post('/usuario', function(req,res){
-  let nombre = req.body.nombre;
-  let body = require.body;
-if(nombre === undefined){
-  res.status(400).json({
-    ok: 400, 
-    mensaje: 'Favor de mandar el valor del nombre'
-  });
-}else{
-
-  res.json({
-    ok: 200,
-    mensaje: 'Usuario insertado con exito',
-    nombre: nombre,
-    body: body
-  });
-}
+mongoose.connect('mongodb://localhost:27017/cafeteria', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+},(err, res) => {
+ if(err) throw err;
+ console.log('Base de datos ONLINE');
 });
  
 app.listen(process.env.PORT, () =>{
-    console.log('El servidor esta en linea en el puerto', process.env.PORT);
+    console.log('El servidor esta en linea por el puerto', process.env.PORT);
 });
